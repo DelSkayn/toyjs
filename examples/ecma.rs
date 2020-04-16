9614f1f4f64a7e5f3afcf74d0788eb6ea68cdaf6
delskayn <mees.delzenne@gmail.com>
2020-04-16 20:35:11 +0200

    Some more error improvements

--- examples/ecma.rs
+++ examples/ecma.rs
@@ -6,12 +6,11 @@ fn run_file(path: &Path) -> bool {
     let mut file = File::open(path).unwrap();
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).unwrap();
-    let l = lexer::Lexer::new(&buffer);
+    let l = lexer::Lexer::new(&buffer, Some(path.to_path_buf()));
     let mut p = parser::Parser::new(l);
     match p.parse_script() {
         Ok(_) => true,
         Err(e) => {
-            println!("in file: {}", path.display());
             println!("{}", e);
             //panic!();
             false
