3da896fa28306022e1f18bb65dff6fc566741dff
delskayn <mees.delzenne@gmail.com>
2020-10-07 21:21:01 +0200

    Added single addition operators

--- crates/runtime/src/gc/trace.rs
+++ crates/runtime/src/gc/trace.rs
@@ -22,7 +22,9 @@ macro_rules! impl_trace_primitive{
     };
 }
 
-impl_trace_primitive!(bool, u8, u16, u32, u64, usize, i8, i16, i32, i64, isize, String,);
+impl_trace_primitive!(
+    bool, char, u8, u16, u32, u64, usize, i8, i16, i32, i64, f32, f64, isize, String, str,
+);
 
 unsafe impl<T: Trace> Trace for Box<T> {
     fn needs_trace() -> bool
@@ -34,6 +36,19 @@ unsafe impl<T: Trace> Trace for Box<T> {
 
     fn trace(&self, ctx: Ctx) {
         (**self).trace(ctx)
+    }
+}
+
+unsafe impl<T: Trace> Trace for Vec<T> {
+    fn needs_trace() -> bool
+    where
+        Self: Sized,
+    {
+        T::needs_trace()
+    }
+
+    fn trace(&self, ctx: Ctx) {
+        self.iter().for_each(|x| x.trace(ctx))
     }
 }
 
