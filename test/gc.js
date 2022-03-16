813c0873b4cadfd51bc49e9db36acdfc61fdf457
delskayn <mees.delzenne@gmail.com>
2022-03-16 16:39:38 +0100

    Improved object literal bytecode generation a bit

--- test/gc.js
+++ test/gc.js
@@ -1,16 +1,18 @@
-for(let i = 100000;i;i--){
-    let a = {
-        b: {
-            c: {
-                d: {
-                    e:{
-                        f:{
+
+for(let j = 10;j;j--){
+    for(let i = 100000;i;i--){
+        let a = {
+            b: {
+                c: {
+                    d: {
+                        e:{
+                            f:{
+                            }
                         }
                     }
                 }
             }
-        }
-    };
-    a.b.c.d.e.f.g = a;
-	console.log(i)
+        };
+        a.b.c.d.e.f.g = a;
+    }
 }
