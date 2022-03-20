efbd2e11e5635aa058f7bcb70eff13f1452b93ee
Mees Delzenne <mees.delzenne@gmail.com>
2022-03-20 16:01:52 +0100

    Misc cleanup

--- crates/parser/tests/parser.rs
+++ crates/parser/tests/parser.rs
@@ -1,5 +1,7 @@
+#![feature(allocator_api)]
+use std::alloc::Global;
+
 use ast::SymbolTable;
-use bumpalo::Bump;
 use common::{interner::Interner, source::Source};
 use lexer::Lexer;
 use toyjs_parser::Parser;
@@ -8,9 +10,8 @@ fn parse(string: &str) {
     let source = Source::from_string(string.to_string());
     let mut interner = Interner::new();
     let lexer = Lexer::new(&source, &mut interner);
-    let bump = Bump::new();
-    let mut variables = SymbolTable::new_in(&bump);
-    Parser::parse_script(lexer, &mut variables, &bump).unwrap();
+    let mut variables = SymbolTable::new_in(Global);
+    Parser::parse_script(lexer, &mut variables, Global).unwrap();
 }
 
 #[test]
