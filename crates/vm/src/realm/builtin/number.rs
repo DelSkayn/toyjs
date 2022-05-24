5522c5a8800d7dbcdfe2c4f08da4d1afe60ca3be
delskayn <mees.delzenne@gmail.com>
2022-05-24 18:38:48 +0200

    Implemented to string for numbers

--- crates/vm/src/realm/builtin/number.rs
+++ crates/vm/src/realm/builtin/number.rs
@@ -8,6 +8,8 @@ use crate::{
     rebind, rebind_try, GcObject, Object, Value,
 };
 
+use super::new_func;
+
 fn construct<'l, 'cell>(
     arena: &'l mut Arena<'_, 'cell>,
     owner: &mut CellOwner<'cell>,
@@ -50,6 +52,81 @@ fn construct<'l, 'cell>(
             ObjectKind::Number(value),
         );
         Ok(rebind!(arena, obj).into())
+    }
+}
+
+fn number_value<'l, 'cell>(
+    arena: &'l mut Arena<'_, 'cell>,
+    owner: &mut CellOwner<'cell>,
+    atoms: &Atoms,
+    realm: GcRealm<'_, 'cell>,
+    this: Value<'_, 'cell>,
+) -> Result<Value<'static, 'cell>, Value<'l, 'cell>> {
+    if let Some(x) = this.into_int() {
+        return Ok(x.into());
+    }
+    if let Some(x) = this.into_float() {
+        return Ok(x.into());
+    }
+    if let Some(obj) = this.into_object() {
+        if let ObjectKind::Number(x) = unsafe { obj.borrow(owner).kind() } {
+            return Ok((*x).into());
+        }
+    }
+    return Err(realm.create_type_error(
+        owner,
+        arena,
+        atoms,
+        "tried to retrieve number value of an object which is neither number nor a Number object",
+    ));
+}
+
+fn value_of<'l, 'cell>(
+    arena: &'l mut Arena<'_, 'cell>,
+    owner: &mut CellOwner<'cell>,
+    atoms: &Atoms,
+    realm: GcRealm<'_, 'cell>,
+    ctx: &ExecutionContext<'_, 'cell>,
+) -> Result<Value<'l, 'cell>, Value<'l, 'cell>> {
+    number_value(arena, owner, atoms, realm, ctx.this)
+}
+
+fn to_string<'l, 'cell>(
+    arena: &'l mut Arena<'_, 'cell>,
+    owner: &mut CellOwner<'cell>,
+    atoms: &Atoms,
+    realm: GcRealm<'_, 'cell>,
+    ctx: &ExecutionContext<'_, 'cell>,
+) -> Result<Value<'l, 'cell>, Value<'l, 'cell>> {
+    let radix = if let Some(x) = realm.arg(owner, 0) {
+        let radix = rebind_try!(arena, realm.to_integer_or_infinity(owner, arena, atoms, x));
+        if radix < 2.0 || radix > 36.0 {
+            //TODO: switch to range error
+            return Err(realm.create_type_error(
+                owner,
+                arena,
+                atoms,
+                format!("{} is not a valid radix", radix),
+            ));
+        }
+        radix as i32
+    } else {
+        10
+    };
+
+    let value = rebind_try!(arena, number_value(arena, owner, atoms, realm, ctx.this));
+    if radix == 10 {
+        realm
+            .to_string(owner, arena, atoms, value)
+            .map(|x| x.into())
+    } else {
+        let value = if let Some(x) = value.into_int() {
+            x as f64
+        } else {
+            value.into_float().unwrap()
+        };
+        let str = GcRealm::to_string_radix(value, radix as u8);
+        Ok(arena.add(str).into())
     }
 }
 
@@ -152,6 +229,28 @@ pub fn init<'l, 'cell>(
         ),
     );
     global.raw_index_set(owner, arena, atoms, atom::constant::Number, construct);
+    let value_of = new_func(arena, owner, atoms, fp, value_of, 0);
+    prototype.raw_index_set_prop(
+        owner,
+        arena,
+        atoms,
+        Property::value(
+            value_of.into(),
+            PropertyFlags::BUILTIN,
+            atom::constant::valueOf,
+        ),
+    );
+    let to_string = new_func(arena, owner, atoms, fp, to_string, 0);
+    prototype.raw_index_set_prop(
+        owner,
+        arena,
+        atoms,
+        Property::value(
+            to_string.into(),
+            PropertyFlags::BUILTIN,
+            atom::constant::toString,
+        ),
+    );
 
     prototype
 }
