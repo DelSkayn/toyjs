ba346504abc824420f9e616348ed11a2bb968a4d
Mees Delzenne <mees.delzenne@gmail.com>
2024-01-25 00:03:33 +0100

    Remove old files

--- toyjs/tests/parseInt.js
+++
@@ -1,9 +0,0 @@
-assert(parseInt("1") === 1);
-assert(parseInt("0x1") === 1);
-assert(parseInt("0xc") === 12);
-assert(parseInt("-0xc") === -12);
-assert(parseInt("-11",2) === -3);
-assert(parseInt("+66",8) === 54);
-assert(isNaN());
-assert(isNaN(parseInt("+ff",37)));
-assert(isNaN(parseInt("+ff",1)));
--- toyjs/tests/this.js
+++
@@ -1,9 +0,0 @@
-function Foo(bar){
-    this.faz = bar
-}
-let f = new Foo("baz");
-assert(f.faz === "baz")
-Foo.prototype.toString = function(){
-    return this.faz + "z";
-}
-assert(f.toString() === "bazz");
